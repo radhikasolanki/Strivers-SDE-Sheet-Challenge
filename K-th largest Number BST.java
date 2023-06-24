11d858dbfe4a32c0860349d503204024c7519d55
radhikasolanki <135941525+radhikasolanki@users.noreply.github.com>
2023-06-24 10:15:09 +0530

    Create K-th largest Number BST.java

---
+++ K-th largest Number BST.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Solution {
+	public static void inorder(TreeNode<Integer> root, ArrayList<Integer> arr){
+		if(root==null){
+			return;
+		}
+		inorder(root.left,arr);
+		arr.add(0,root.data);
+		inorder(root.right,arr);
+	}
+	public static int KthLargestNumber(TreeNode<Integer> root, int k) {
+		// Write your code here.
+		if(root==null || k==0)return -1;
+		ArrayList<Integer> arr=new ArrayList<>();
+		inorder(root,arr);
+		if(arr.size()<k)return-1;
+		return arr.get(k-1);
+	}
+}
