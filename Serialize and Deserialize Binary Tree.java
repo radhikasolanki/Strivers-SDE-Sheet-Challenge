c1d139603ef045ad31b342c168f1b248d6742bf8
radhikasolanki <135941525+radhikasolanki@users.noreply.github.com>
2023-07-01 13:32:22 +0530

    Create Serialize and Deserialize Binary Tree.java

---
+++ Serialize and Deserialize Binary Tree.java
@@ -0,0 +1,34 @@
+/************************************************************
+
+    Following is the TreeNode class structure
+
+    template <typename T>
+    class TreeNode {
+       public:
+        T data;
+        TreeNode<T> *left;
+        TreeNode<T> *right;
+
+        TreeNode(T data) {
+            this->data = data;
+            left = NULL;
+            right = NULL;
+        }
+    };
+
+************************************************************/
+
+string serializeTree(TreeNode<int> *root)
+{
+ //    Write your code here for serializing the tree
+
+}
+
+TreeNode<int>* deserializeTree(string &serialized)
+{
+ //    Write your code here for deserializing the tree
+
+}
+
+
+
